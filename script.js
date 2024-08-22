f2fbaa60beed4377fa6f6462d7c8e55bced97857
vikas <rawatvikas689@gmail.com>
2024-08-22 20:47:29 +0530

    commit

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function preloadImages() {
         loadImage(frames.currentIndex);
         startAnimation();asdf
       }jsdfa
-    };sdfasdfsdfsdfdfs
+    };sdfasdfsdfsdfdfsdf
     images.push(img);
   }
 }
